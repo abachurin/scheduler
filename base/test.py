ff0ed9afe597ab2e59aefd9c689d259b4bb0fa33
Alex Bachurin <bachurin.alex@gmail.com>
2022-05-09 12:44:37 +0100

    *

--- base/test.py
+++
@@ -1,8 +0,0 @@
-from .scheduler import *
-
-name = 'extract_iz_files_from_mail'
-
-with open('config.json', 'r', encoding='utf-8') as f:
-    config = json.load(f)
-args = config.get(name, None)
-globals()[name](args)
---
+++ test.py
@@ -0,0 +1,16 @@
+from base.scheduler import *
+
+try:
+    import win32com.client as win32
+
+    xl_app = win32.DispatchEx("Excel.Application")
+    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
+except Exception as ex:
+    print("No Windows module")
+
+name = 'extract_iz_files_from_mail'
+
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+args = config.get(name, None)
+globals()[name](args)
